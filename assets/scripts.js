455a667f9d05adcca6dc74f38bca48c74a893f2e
Mbaouz <145072570+Mbaouz@users.noreply.github.com>
2024-04-21 23:37:13 +0200

    accessibilté validé

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
     $('.gallery').mauGallery({
         columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
+            xs: 1,sm: 2,md: 3,lg: 3,xl: 3
         },
         lightBox: true,
         lightboxId: 'myAwesomeLightbox',
